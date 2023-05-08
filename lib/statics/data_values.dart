70aff13a12e871b16fec10d9f6e9f167b9a7a64e
eeimaan <emanyaqoob379@gmail.com>
2023-05-08 23:48:50 +0500

    Initial commit

---
+++ lib/statics/data_values.dart
@@ -0,0 +1,203 @@
+class DataValues {
+  // About App
+  static const String appName = 'Emaan Yaqoob';
+  static const String appNameLong = 'Emaan Yaqoob.dev';
+  static const String appDescription =
+      'Hello there, I\'m Emaan yaqoob. This is my personal website, which contains public information that anyone can find on the internet.';
+  static const String appDeveloper = 'Emaan Yaqoob';
+ static const String appVersion = '1.1.2';
+  static const String appBuildNumber = '1';
+
+  // External links
+  static Uri devURL = Uri.parse('https:');
+  static Uri hashnodeURL = Uri.parse('https:');
+  static Uri githubURL = Uri.parse('https://github.com/eeimaan');
+  static Uri linkedinURL = Uri.parse('https://linkedin.com/in/emaanyaqub');
+  static Uri twitterURL = Uri.parse('https://');
+  static Uri youtubeURL = Uri.parse('https:');
+  static Uri telegramURL = Uri.parse('https');
+  static Uri facebookURL = Uri.parse('https://');
+  static Uri instagramURL = Uri.parse('https://instagram.com/herimaan_');
+  static Uri repoURL =
+      Uri.parse('https://github.com/');
+  static Uri biographyURL = Uri.parse('https:/');
+  static Uri profilesURL = Uri.parse('https://profile.dev');
+  static Uri toolkitURL = Uri.parse('https://.dev');
+  static Uri volunteeringURL =
+      Uri.parse('https://');
+  static Uri technotesURL = Uri.parse('https://medium.com/@emanyaqoob');
+  static Uri lateNightStudentURL = Uri.parse('https://medium.com/@emanyaqoob/dart-list-tutorial-with-examples-e55301323722');
+
+  // Header
+  static const String headerGreetings = 'Hello, World! I\'m';
+  static const String headerName = 'Emaan Yaqoob';
+  static const String headerTitle = 'Student | Developer | Volunteer';
+
+  // Navigation Bar
+  static const String navBarAboutMe = 'About Me';
+  static const String navBarEducation = 'Education';
+  static const String navBarExperience = 'Experience';
+  static const String navBarVolunteering = 'Volunteering';
+  static const String navBarTechNotes = 'TechNotes';
+  static const String navBarContactMe = 'Contact Me';
+
+  // About me
+  static const String aboutMeTitle = 'About Me';
+  static const String aboutMeDescription =
+      'This website includes my public details, which anyone can find on the internet. So if you are looking for my personal information, such as my bio, education, experience, and other things that may make you wonder, feel free to scroll up and down to find out about me.';
+  static const String aboutMeBiographyTitle = 'Biography';
+  static const String aboutMeBiographyDescription =
+      'Hi there! I\'m a computing student, Flutter developer, and community volunteer. I believe that personal growth and learning are essential for success, and I have dedicated my life to achieving both. '
+      'Because of that, I\'m always looking to learn new things and apply them to whatever I\'m doing. '
+      'Technology is always igniting my passion for the positive impact it can have, and I\'m excited to continue growing my skills and knowledge, so I started my career as a techie. '
+      'In addition to my studies, I\'m also passionate about giving back to various communities and helping others, which is why I volunteer my time to help out whenever I can, and I believe that this work is vital in helping to create a better future for everyone.';
+  static const String aboutMeFullNameTitle = 'Full Name';
+  static const String aboutMeFullNameDescription =
+      'Emaan Yaqoob ';
+  static const String aboutMeNwITitle = 'Name with Initials';
+  static const String aboutMeNwIDescription = 'Emaan yaqoob flutter dev';
+  static const String aboutMeFnLTitle = 'First and Last Name';
+  static const String aboutMeFnLDescription = 'Emaan yaqoob';
+  static const String aboutMeDobTitle = 'Birthday';
+  static const String aboutMeDobDescription = 'July 29, 2001';
+  static const String aboutMeLanguageTitle = 'Languages';
+  static const String aboutMeLanguageDescription = 'Urdu (Native)\n'
+      'English (Professional)';
+  static const String aboutMeNationalityTitle = 'Nationality';
+  static const String aboutMeNationalityDescription = 'Pakistani';
+  static const String aboutMeGenderTitle = 'Gender';
+  static const String aboutMeGenderDescription = 'Female (She/Her)';
+  static const String aboutMeLocationTitle = 'Current Locations';
+  static const String aboutMeLocationDescription =
+      'Bahawalpur, Punjab, Pakistan';
+  static const String aboutMeWorkDomainTitle = 'Working Domain';
+  static const String aboutMeWorkDomainDescription = 'emaan yaqoob.dev\n'
+      '08:00 - 18:00 (GMT+5:30)';
+  static const String aboutMeStudentTitle = 'Computing Student';
+  static const String aboutMeStudentDescription =
+      'I am a graduate in Bachelor of Computer Science with a strong background in Android development. I am skilled in Java, C++, and Dart, and I have recently been learning Flutter on my own, developing several successful apps in the process. With expertise in database and data structures. I am always up to date with the latest technologies and news, and am an active member of various tech communities.';
+  static const String aboutMeDeveloperTitle = 'Flutter Developer';
+  static const String aboutMeDeveloperDescription =
+      'My experience also includes developing mobile applications, integrating APIs, integrating Google Maps, creating and managing local databases, and creating products from scratch. I am a quick learner, excellent at time management, always punctual and reliable, a keen observer, possess a positive attitude and excel in problem-solving.';
+  static const String aboutMeVolunteerTitle = 'Community Volunteer';
+  static const String aboutMeVolunteerDescription =
+      'I\'ve been a volunteer for many years and have had the opportunity to make a difference in my social and technical communities. Volunteering has allowed me to learn more about the people and things around me while also making a positive difference in the lives of those I serve, and by volunteering, I would love to help others reach their life goals.';
+  static const String aboutMeHobbiesTitle = 'Hobbies';
+  static const String aboutMeHobbiesDescription = 'Learning New Things\n'
+      'Video Editing\n'
+      'Art\n'
+      'Content Creation';
+  static const String aboutMeGoalTitle = 'Goal';
+  static const String aboutMeGoalDescription =
+      'To grow personally and professionally, while making a positive impact on the community.';
+  static const String aboutMeBanner = 'You can find my';
+  static const String aboutMeBannerTitle = 'public profiles';
+  static const String aboutMeBannerWeb =
+      'Visit profile.EmaanYaqoob to find all my public profiles.';
+
+  // Education
+  static const String educationTitle = 'Education';
+  static const String educationDescription =
+      'My educational background is here in detail';
+  static const String educationOrg1Title =
+      'Superior College Bahawalpur';
+  static const String educationOrg1Course1Name = 'ICS';
+  static const String educationOrg1Course1Grade =
+      ' Maths, Physics, and Computer';
+  static const String educationOrg1Course1Year = 'Attempted: 2016 & 2018';
+  static const String educationOrg1Course2Name = 'Matric ';
+  static const String educationOrg1Course2Grade = 'Passed with 82%';
+  static const String educationOrg1Course2Year = '2015';
+  static const String educationOrg1Course3Name = 'Education Period';
+  static const String educationOrg1Course3Year = 'Secondary: 2008 - 2016';
+  static const String educationOrg1Course3Year2 = 'Primary: 2003 - 2007';
+
+  static const String educationOrg2Title = 'The Islamia University Bahawalpur';
+  static const String educationOrg2Course1Name = 'BS Computer Science';
+  static const String educationOrg2Course1Grade =
+      'awarded by IUB , 3.6 GPA';
+  static const String educationOrg2Course1Year = '2018 - 2022';
+  static const String educationOrg2Course2Name =
+      'Online Learning Platforms';
+  static const String educationOrg2Course2Grade =
+      '';
+  static const String educationOrg2Course2Year = '';
+  static const String educationOrg2Course3Name =
+      '';
+  static const String educationOrg2Course3Grade =
+      '';
+  static const String educationOrg2Course3Year = '';
+  static const String educationBanner = '';
+  static const String educationBannerTitle = '';
+  static const String educationBannerWeb =
+      'Click here to find out more about my online learning platforms.';
+
+  // Experience
+  static const String experienceTitle = 'Experience';
+  static const String experienceDescription =
+      'Since I started Android Development in 2020, I\'ve started to learn and apply my knowledge in different places. So I started working as a freelance solution developer, and with time, I fell in love with different technologies. now I try out my experience.';
+  static const String experienceOrg1Title = 'CAS';
+  static const String experienceOrg1Role = 'Android Developer';
+  static const String experienceOrg1Years = 'June 2019 - June 2022';
+  static const String experienceOrg1Vales =
+      'Mobile, Desktop, Web development, UI UX, Tech Support Assistant & Data Analysis.';
+  static const String experienceOrg2Title = 'Online platform';
+  static const String experienceOrg2Role =
+      'Trainee Software Developer (Flutter)';
+  static const String experienceOrg2Years = 'November 2022- ongoing';
+  static const String experienceOrg2Vales =
+      'Android, Flutter, Dart, Firebase, Git, REST API, JSON Parsing & UI UX Designing.';
+  static const String experienceBanner = 'Find my working';
+  static const String experienceBannerTitle = 'Github';
+  static const String experienceBannerWeb =
+      'Visit Github for more details >>';
+
+  // Volunteering
+  static const String volunteeringTitle = 'Volunteering';
+  static const String volunteeringDescription =
+      'Volunteering is an amazing way for me to give back to communities and make a difference. It\'s also a terrific opportunity to meet new people, learn new skills, and gain valuable experience. I do volunteer work for many different social and technical organizations. These are more focused organizations where I am presently volunteering.';
+  static const String volunteeringOrg1Title = 'FlutterScope';
+  static const String volunteeringOrg1Role = 'Content Creator';
+  static const String volunteeringOrg1Years = ' 2022 ';
+  static const String volunteeringOrg1Vales =
+      'FlutterScope was founded to disseminate knowledge about Flutter and related technologies. This community is an educational group that is not affiliated with or otherwise sponsored by Google LLC.';
+  static const String volunteeringOrg2Title = 'GDSC IUB';
+  static const String volunteeringOrg2Role =
+      'Flutter Development lead';
+  static const String volunteeringOrg2Years = 'October 2021 - 2022';
+  static const String volunteeringOrg2Vales =
+      'this is  a global group of campus leaders who are eager to help fellow students, create robust tech communities, and develop technical and career skills for the future.';
+  static const String volunteeringBanner = 'Find more about my';
+  static const String volunteeringBannerTitle = 'volunteering';
+  static const String volunteeringBannerWeb =
+      'Visit volunteer.Emaan Yaqoob.dev for more details >>';
+
+  // TechNotes
+  static const String technotesTitle = 'TechNotes on medium';
+  static const String technotesDescription =
+      'I love creating content as a hobby! It\'s a fantastic outlet for my creativity, and I enjoy expressing myself and connecting with people who share my interests. I have a blog where I can share my written ideas, and a vlog where my voice can be heard. It\'s a great way to connect with others and share my passion for technology and all things digital. Join me on this exciting journey of exploration and discovery in the tech world!';
+  static const String technotesOrg1Title = ' Dart List Tutorial with Examples';
+  static const String technotesOrg1Role =
+      'List is one of the most popular data structure in OOP . In this tutorial, I’ll show you many methods to work with a List in Dart..';
+  static const String technotesOrg1Years = '2022  - Present';
+  static const String technotesOrg1Vales =
+      'Welcome to my tech blog, where I share my tech notes! As a passionate tech enthusiast, I\'m always on the lookout for the latest and most innovative advancements in the industry, and I can\'t wait to share my findings with you. In this space, we\'ll dive deep into a variety of exciting tech topics, ranging from revolutionary software solutions to groundbreaking ideas and futuristic concepts. Along the way, I\'ll share my personal experiences and provide insights on how these developments impact our world. Join me on this exciting journey of discovery and exploration in the dynamic world of technology through this blog!';
+  static const String technotesOrg2Title = 'Flutter: State Management in flutter';
+  static const String technotesOrg2Role =
+      'In this article, we will explore an overview of Flutter State Management.';
+  static const String technotesOrg2Years = 'Coming Soon';
+  static const String technotesOrg2Vales =
+      'Welcome to my vlog, where we\'ll be exploring the fascinating world of technology! Technology is constantly evolving and transforming the world we live in, creating new paths and opportunities for us to explore. In this vlog, I\'ll be sharing my personal journey of discovery and exploration in the tech world, as I learn about the latest advancements and emerging trends in the industry. From cutting-edge software and innovative solutions to groundbreaking ideas and futuristic concepts, we\'ll cover it all in this vlog. So join me on this exciting journey as we discover the ever-changing world of technology together!';
+  // Contact
+  static const String contactTitle = 'Contact';
+  static const String contactDescription =
+      'Hey! I appreciate you looking at my public profile. Please feel free to ask me anything or to discuss anything with me. I\'m happy to chat with and meet new people. Regardless of the fact that I don\'t use social media much, you can find me there. So find me and get in touch with me. Send me an email if you need to discuss anything serious with me. I\'ll get back to you in 12 to 24 hours.';
+  static const String contactBanner = 'Send me an email at';
+  static const String contactEmail = 'emanyaqoob379@gmail.com';
+
+  // Footer
+  static const String copyright =
+      "Copyright ©2023 Emaan Yaqoob. All Rights Reserved.";
+
+  static const String builtWith = "Built with Flutter and GitHub Pages.";
+}
